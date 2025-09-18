6459b3ec75e4487cb31ff9e3cc4736e409e18526
XuanSang1208 <sang.truong2005@hcmut.edu.vn>
2025-09-18 21:20:57 +0700

    First commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),TanStackRouterVite(), 
+],
   server:
   {
     port: 3000
