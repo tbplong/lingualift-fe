decce76673c0b89c1c25a609598c849d15b1382a
GitGud031005 <nguyenphuc102005@gmail.com>
2025-09-20 18:34:17 +0700

    Feat/setup (#8)
    * feat: setup tailwind v4
    
    * fix: branch name & server port
    
    * chore: add signature
    
    * First commit
    
    * feat: setup tanstack router, zustand, axios, twMerge, husky, lint-staged, commitlint
    
    * fix: Update pakage.json
    
    ---------
    
    Co-authored-by: Truong Nam <86135739+TruongMarco1305@users.noreply.github.com>
    Co-authored-by: XuanSang1208 <sang.truong2005@hcmut.edu.vn>

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server:
-  {
-    port: 3000
-  }
-})
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
+  server: {
+    port: 3000,
+  },
+});
