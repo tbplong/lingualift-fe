0afe7545775439ffae001ad75d36d41c1089b1c5
XuanSang2005 <sangbom2005@gmail.com>
2025-09-23 03:43:16 +0700

    Update Body.tsx

--- src/components/landingPage/Body.tsx
+++ src/components/landingPage/Body.tsx
@@ -14,25 +14,18 @@ import { TiktokWhite } from "../icons";
 export function Connect() {
   return (
     <section className="flex flex-col items-center py-10 mt-85">
-      {/* Tiêu đề */}
       <h2 className="text-8xl font-bold">
         <span className="text-secondary">KẾT NỐI</span>{" "}
         <span className="text-gray-700">VỚI</span>{" "}
         <span className="text-primary">THẦY LUÂN</span>
       </h2>
 
-      {/* Hộp chứa icon */}
       <div className="relative mt-15">
-        {/* Bóng nền vàng */}
         <div className="absolute -bottom-4 left-2 w-452 h-85 bg-secondary rounded-md"></div>
 
-        {/* Box chính */}
         <div className="relative bg-primary rounded-md p-6 flex gap-14 w-450 h-85 justify-center items-center">
-          {/* Facebook */}
           <a className="group relative w-55 h-55 flex items-center justify-center rounded-xl">
-            {/* border nền mờ */}
             <span className="absolute inset-0 rounded-xl border border-white bg-blue-600" />
-            {/* dải quét */}
             <span className="absolute inset-0 rounded-xl overflow-hidden pointer-events-none">
               <span className="absolute -left-full top-0 h-full w-1/2 bg-white/10 skew-x-[-20deg] transition-transform duration-500 group-hover:translate-x-[200%]" />
             </span>
@@ -43,11 +36,8 @@ export function Connect() {
             </div>
           </a>
 
-          {/* Messenger */}
           <a className="group relative w-55 h-55 flex items-center justify-center rounded-xl ">
-            {/* border nền mờ */}
             <span className="absolute inset-0 rounded-xl border border-white bg-blue-600" />
-            {/* dải quét */}
             <span className="absolute inset-0 rounded-xl overflow-hidden pointer-events-none">
               <span className="absolute -left-full top-0 h-full w-1/2 bg-white/10 skew-x-[-20deg] transition-transform duration-500 group-hover:translate-x-[200%]" />
             </span>
@@ -58,11 +48,8 @@ export function Connect() {
             </div>
           </a>
 
-          {/* TikTok */}
           <a className="group relative w-55 h-55 flex items-center justify-center rounded-xl ">
-            {/* border nền mờ */}
             <span className="absolute inset-0 rounded-xl border border-white bg-blue-600" />
-            {/* dải quét */}
             <span className="absolute inset-0 rounded-xl overflow-hidden pointer-events-none">
               <span className="absolute -left-full top-0 h-full w-1/2 bg-white/10 skew-x-[-20deg] transition-transform duration-500 group-hover:translate-x-[200%]" />
             </span>
@@ -74,9 +61,7 @@ export function Connect() {
           </a>
 
           <a className="group relative w-55 h-55 flex items-center justify-center rounded-xl ">
-            {/* border nền mờ */}
             <span className="absolute inset-0 rounded-xl border border-white bg-blue-600" />
-            {/* dải quét */}
             <span className="absolute inset-0 rounded-xl overflow-hidden pointer-events-none">
               <span className="absolute -left-full top-0 h-full w-1/2 bg-white/10 skew-x-[-20deg] transition-transform duration-500 group-hover:translate-x-[200%]" />
             </span>
