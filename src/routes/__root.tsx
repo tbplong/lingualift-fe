2b8f6cfd333678e76cafbf13f31dfc5706877977
XuanSang2005 <sangbom2005@gmail.com>
2025-09-26 00:08:44 +0700

    fix: update hero

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,20 +1,25 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { createRootRoute } from "@tanstack/react-router";
+import { Header } from "../components/Header";
+import Hero from "../pages/Landing/Hero";
+import { Connect } from "../pages/Landing/Connect";
+import { FooterBadgeRow } from "../pages/Landing/FooterBadge";
+import { Footer } from "../components/Footer";
+import CoreValue from "../pages/Landing/CoreValue";
+import WhyChoose from "../pages/Landing/WhyChoose";
+
+// import DashboardSection from "../components/landingPage/Dashboard";
 
 const RootLayout = () => (
-  <>
-    <div className="p-2 flex gap-2">
-      <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>{" "}
-      <Link to="/about" className="[&.active]:font-bold">
-        About
-      </Link>
-    </div>
-    <hr />
-    <Outlet />
-    <TanStackRouterDevtools />
-  </>
+  <div className="min-h-screen flex flex-col overflow-hidden">
+    <Header />
+    <Hero />
+    {/* <DashboardSection/> */}
+    <CoreValue />
+    <WhyChoose />
+    <Connect />
+    <FooterBadgeRow />
+    <Footer />
+  </div>
 );
 
 export const Route = createRootRoute({ component: RootLayout });
