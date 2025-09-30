c8a08bb18ee0d65f4ae1f3c2dc19e73e66f6c826
GitGud031005 <nguyenphuc102005@gmail.com>
2025-09-30 22:51:16 +0700

    chore: chore

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import path from "node:path";
 
 // https://vite.dev/config/
