2bb3c98d7ada7daa55ba8180d743acd4437024fd
GitGud031005 <nguyenphuc102005@gmail.com>
2025-10-02 17:25:31 +0700

    fix: final responsive core value

--- src/pages/Landing/CoreValue.tsx
+++ src/pages/Landing/CoreValue.tsx
@@ -1,58 +1,146 @@
+import CoreValueCard from "./CoreValueCard.tsx";
 import Marquee from "./Marquee";
+import "./CoreValue.css";
+import { useEffect, useRef } from "react";
+import { autoScroll } from "./autoScroll.ts";
 const CoreValue = () => {
+  const trackRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!trackRef.current) return;
+    const stop = autoScroll(trackRef.current, 3000); // every 3s
+    return stop; // cleanup when unmount
+  }, []);
+
   return (
-    <section className="h-dvh flex flex-col items-center">
-      <div className="mt-16 text-secondary text-5xl font-bold">
+    <section className="flex flex-col items-center mb-20">
+      <div className="mt-16 text-secondary lg:text-5xl text-3xl font-bold">
         <span className="text-primary">Our Core</span> Values
       </div>
-      <div className="flex gap-3 my-6">
+      <div className="flex gap-3 lg:my-6 my-5">
         <div className="h-2 w-2 bg-tertiary-100 rounded-full"></div>
         <div className="h-2 w-10 bg-secondary-300 rounded-full"></div>
         <div className="h-2 w-2 bg-tertiary-100 rounded-full"></div>
       </div>
-      <p className="mb-8 w-4xl text-xl text-center text-tertiary">
+      <p className="lg:mb-8 mb-0 xl:w-4xl w-4/5 lg:text-xl text-base text-center text-tertiary">
         LinguaLift combines learner-first teaching, clear instruction, and
         community support with exam-focused practice and personalised feedback
         to help students aim for a 9+.
       </p>
-      <div className="flex gap-8 p-6 mb-42">
-        <div className="card rounded-2xl bg-white w-120 h-160 shadow-lg text-tertiary border-2 border-[#00CAC7]">
-          <figure className="h-4/5 w-full p-4 rounded-2xl">
-            <img className="h-full rounded-xl" src="/exam.jpg" />
-          </figure>
-          <div className="card-body p-4 pt-2 gap-0">
-            <h2 className="card-title text-2xl ">Mock Exams</h2>
-            <p className="text-lg leading-6">
-              Real exam-style tests that build confidence and show you exactly
-              where to improve.
-            </p>
-          </div>
-        </div>
-        <div className="card rounded-2xl bg-white w-120 h-160 shadow-lg text-tertiary border-2 border-[#FDDED9]">
-          <figure className="h-4/5 w-full p-4 rounded-2xl">
-            <img className="h-full rounded-xl" src="/planner.jpg" />
-          </figure>
-          <div className="card-body p-4 pt-2 gap-0">
-            <h2 className="card-title text-2xl ">Study Planner</h2>
-            <p className="text-lg leading-6">
-              Set goals, follow a daily plan, and stay on track until exam day.
-            </p>
-          </div>
-        </div>
-        <div className="card rounded-2xl bg-white w-120 h-160 shadow-lg text-tertiary border-2 border-[#DEE2FF]">
-          <figure className="h-4/5 w-full p-4 rounded-2xl">
-            <div className="bg-[#DEE2FF] w-full h-full flex items-center justify-center rounded-xl">
+      <div
+        ref={trackRef}
+        className="carousel flex lg:gap-8 gap-4 2xl:w-4/5 xl:w-9/10 lg:w-9/10 w-8/10 lg:aspect-[1504/666.725] p-6 overflow-x-auto snap-x snap-mandatory"
+      >
+        <CoreValueCard
+          color="[#00CAC7]"
+          img="/exam.jpg"
+          title="Mock Exams"
+          content="Real exam-style tests that build confidence and show you exactly where to improve."
+        />
+        <CoreValueCard
+          color="[#FDDED9]"
+          img="/planner.jpg"
+          title="Study Planner"
+          content="Set goals, follow a daily plan, and stay on track until exam day with confidence."
+        />
+        <CoreValueCard
+          color="[#DEE2FF]"
+          title="Flashcards"
+          content="Easy “remember cards” to review words, phrases, and grammar on the go."
+          comp={
+            <div className=" w-full h-full flex items-center justify-center rounded-xl">
               <img className="h-1/2 rounded-xl" src="/flash.png" />
             </div>
-          </figure>
-          <div className="card-body p-4 pt-2 gap-0">
-            <h2 className="card-title text-2xl ">Flashcards</h2>
-            <p className="text-lg leading-6">
-              Easy “remember cards” to review words, phrases, and grammar on the
-              go.
-            </p>
-          </div>
-        </div>
+          }
+        />
+        <CoreValueCard
+          color="[#00CAC7]"
+          img="/exam.jpg"
+          title="Mock Exams"
+          content="Real exam-style tests that build confidence and show you exactly where to improve."
+        />
+        <CoreValueCard
+          color="[#FDDED9]"
+          img="/planner.jpg"
+          title="Study Planner"
+          content="Set goals, follow a daily plan, and stay on track until exam day with confidence."
+        />
+        <CoreValueCard
+          color="[#DEE2FF]"
+          title="Flashcards"
+          content="Easy “remember cards” to review words, phrases, and grammar on the go."
+          comp={
+            <div className=" w-full h-full flex items-center justify-center rounded-xl">
+              <img className="h-1/2 rounded-xl" src="/flash.png" />
+            </div>
+          }
+        />
+        <CoreValueCard
+          color="[#00CAC7]"
+          img="/exam.jpg"
+          title="Mock Exams"
+          content="Real exam-style tests that build confidence and show you exactly where to improve."
+        />
+        <CoreValueCard
+          color="[#FDDED9]"
+          img="/planner.jpg"
+          title="Study Planner"
+          content="Set goals, follow a daily plan, and stay on track until exam day with confidence."
+        />
+        <CoreValueCard
+          color="[#DEE2FF]"
+          title="Flashcards"
+          content="Easy “remember cards” to review words, phrases, and grammar on the go."
+          comp={
+            <div className=" w-full h-full flex items-center justify-center rounded-xl">
+              <img className="h-1/2 rounded-xl" src="/flash.png" />
+            </div>
+          }
+        />
+        <CoreValueCard
+          color="[#00CAC7]"
+          img="/exam.jpg"
+          title="Mock Exams"
+          content="Real exam-style tests that build confidence and show you exactly where to improve."
+        />
+        <CoreValueCard
+          color="[#FDDED9]"
+          img="/planner.jpg"
+          title="Study Planner"
+          content="Set goals, follow a daily plan, and stay on track until exam day with confidence."
+        />
+        <CoreValueCard
+          color="[#DEE2FF]"
+          title="Flashcards"
+          content="Easy “remember cards” to review words, phrases, and grammar on the go."
+          comp={
+            <div className=" w-full h-full flex items-center justify-center rounded-xl">
+              <img className="h-1/2 rounded-xl" src="/flash.png" />
+            </div>
+          }
+        />
+        <CoreValueCard
+          color="[#00CAC7]"
+          img="/exam.jpg"
+          title="Mock Exams"
+          content="Real exam-style tests that build confidence and show you exactly where to improve."
+        />
+        <CoreValueCard
+          color="[#FDDED9]"
+          img="/planner.jpg"
+          title="Study Planner"
+          content="Set goals, follow a daily plan, and stay on track until exam day with confidence."
+        />
+        <CoreValueCard
+          color="[#DEE2FF]"
+          title="Flashcards"
+          content="Easy “remember cards” to review words, phrases, and grammar on the go."
+          comp={
+            <div className=" w-full h-full flex items-center justify-center rounded-xl">
+              <img className="h-1/2 rounded-xl" src="/flash.png" />
+            </div>
+          }
+        />
       </div>
 
       <Marquee />
