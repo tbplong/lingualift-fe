75cacfbd13ba79c31c6fec960254138d04892d73
GitGud031005 <nguyenphuc102005@gmail.com>
2025-10-04 12:55:59 +0700

    fix: why choose flex

--- src/pages/Landing/WhyChooseCard.tsx
+++ src/pages/Landing/WhyChooseCard.tsx
@@ -9,7 +9,7 @@ const WhyChooseCard = ({ title, content, icon }: CardProps) => {
     <div className="card card-border flex-1 bg-tertiary-000 rounded-4xl max-lg:rounded-2xl h-7/8 max-lg:h-full max-lg:aspect-square">
       <div className="card-body 2xl:p-6 xl:py-5 md:py-3 md:pl-4 sm:py-4 sm:pl-4 py-1 pl-2 pr-1">
         <div className="card-actions justify-start xl:h-1/3 h-9/30 2xl:mb-2 max-xl:mt-1">
-          <div className="4xl:w-20 3xl:w-18 2xl:w-16 lg:w-15 sm:w-20 xs:w-13 w-10 aspect-square rounded-full bg-white flex items-center justify-center">
+          <div className="4xl:w-19 3xl:w-18 2xl:w-17 lg:w-12 sm:w-20 xs:w-13 w-10 aspect-square rounded-full bg-white flex items-center justify-center">
             <img className="w-5/10" src={icon}></img>
           </div>
         </div>
