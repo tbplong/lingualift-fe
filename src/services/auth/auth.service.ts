4fa62ef5de47d1eb6278ddc5223ff37d0e25e2c6
XuanSang2005 <sangbom2005@gmail.com>
2025-10-22 03:09:19 +0700

    feat: api

---
+++ src/services/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import axios from "@/utils/custom-axios";
+const AuthService = {
+  login: async (email: string, password: string) => {
+    return await axios.post<{
+      accessToken: string;
+    }>(`auth/login`, {
+      email,
+      password,
+    });
+  },
+  signup: async (
+    email: string,
+    password: string,
+    firstName: string,
+    lastName: string,
+    dateofBirth: string,
+    phone: string,
+  ) => {
+    console.log("Signup is called");
+    return await axios.post<{
+      accessToken: string;
+    }>(`auth/signup`, {
+      email: email,
+      password: password,
+      firstName: lastName,
+      dateofBirth: dateofBirth,
+      phone: phone,
+    });
+  },
+};
+
+export default AuthService;
